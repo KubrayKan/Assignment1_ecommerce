5f66ed7ad21d6c714268c89c05d24009d497282a
Odysseas Kotsirilos <odysseaskotsirilos@yahoo.gr>
2020-09-09 22:52:28 -0400

    Official intitial commit

---
+++ Assignment1_ecommerce/masterPage/navAndSearchBar.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment1_ecommerce.masterPage
+{
+    public partial class navAndSearchBar : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
