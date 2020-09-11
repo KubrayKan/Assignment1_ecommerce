00a770c21526b5954a9359917b3a05cdc0526915
Odysseas Kotsirilos <odysseaskotsirilos@yahoo.gr>
2020-09-10 22:27:09 -0400

    Added a few db lines of code.

--- Assignment1_ecommerce/home.aspx.cs
+++ Assignment1_ecommerce/home.aspx.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Assignment1_ecommerce
 {
     public partial class home : System.Web.UI.Page
     {
+        private string backgroundImage;
+        private string image1, image2, image3;
+        private string holidaysImage, bestTimeImage, safarisImage; 
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection myConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\odysseas\Desktop\Assignment1_ecommerce\Assignment1_ecommerce\App_Data\projectDB.mdf;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand("select * from <table>", myConnection);
+                myConnection.Open();
 
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                while(dataReader.Read())
+                {
+
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.ToString());
+            }
         }
     }
 }
